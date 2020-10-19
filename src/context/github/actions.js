60cbbe1123ef52c27a7d1610da44434835dceae4
Bernardo-Quina <bernardoquina@gmail.com>
2020-10-19 18:06:39 +0100

    Actions file typo: http to https

--- src/context/github/actions.js
+++ src/context/github/actions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const github = axios.create({
-  baseURL: 'http://api.github.com',
+  baseURL: 'https://api.github.com',
   timeout: 1000,
   headers: { Authorization: process.env.REACT_APP_GITHUB_TOKEN }
 })
